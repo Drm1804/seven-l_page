d676091f6fe148e5eae5f86e66b2e42526ae3406
Drm1804 <drm-a@yandex.ru>
2015-10-06 16:20:23 +0500

    Внес правки от клиента

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,26 @@
-$(document).ready(function(){
-    $('.b-sec-part-box__cat').on('click', function(){
-        $('.b-sec-part-box__cat').each(function(){
-            $(this).removeClass('b-sec-part-box__cat--check');
-            $(this).siblings('.b-sec-cat-box').removeClass('b-sec-cat-box--show')
-        });
-        $(this).toggleClass('b-sec-part-box__cat--check').siblings('.b-sec-cat-box')
-            .toggleClass('b-sec-cat-box--show');
+$(document).ready(function () {
+  $('.b-sec-part-box__cat').on('click', function () {
+    $('.b-sec-part-box__cat').each(function () {
+      $(this).removeClass('b-sec-part-box__cat--check');
+      $(this).siblings('.b-sec-cat-box').removeClass('b-sec-cat-box--show')
+    });
+    $(this).toggleClass('b-sec-part-box__cat--check').siblings('.b-sec-cat-box')
+      .toggleClass('b-sec-cat-box--show');
+  });
+  $('.b-sec-part-box-li').hover(function(){
+    var num = $(this).attr('data-num');
+    $('.b-sec-main-part-layer').each(function(){
+      if($(this).attr('data-num') === num){
+        $(this).css('background-color', '#ff5400')
+      }
     })
+  },
+  function(){
+    $('.b-sec-main-part-layer').each(function(){
+      $(this).css('background-color', '#fff')
+    })
+  })
+
+  $('.fancybox').fancybox();
+
 });
